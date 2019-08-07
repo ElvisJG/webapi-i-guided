a603e3e1b8c57f553fa5e898b0129b7a70c6b8c2
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-06 20:32:32 -0400

    added /hubs

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const db = require('./data/hubs-model.js');
 
 const server = express();
+
+// request handler
+
+// GET /
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Get /now
+// send back a timestamp
+server.get('/now', (req, res) => {
+  const now = new Date().toLocaleTimeString();
+  res.send(now);
+});
+
+// GET /hubs
+server.get('/hubs', (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.json(hubs);
+    })
+    .catch(err => {
+      res.status(500).json({ err: err });
+    });
+});
 
 // Should be the last step
 server.listen(4000, () => {
