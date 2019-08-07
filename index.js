4c84de8a61c00fd4fbd8ed812fff152655b6ac5f
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-07 18:50:32 -0400

    wip

--- index.js
+++ index.js
@@ -21,10 +21,80 @@ server.get('/now', (req, res) => {
 server.get('/hubs', (req, res) => {
   db.find()
     .then(hubs => {
+      //   console.log('hubs', hubs);
       res.json(hubs);
     })
     .catch(err => {
       res.status(500).json({ err: err });
+    });
+});
+
+server.post('/hubs', (req, res) => {
+  const newHub = req.body;
+  // console.log('new hub', newHub);
+  // validate the hub
+  db.add(newHub)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to create a new hub'
+      });
+    });
+});
+
+// DESTROY!!!! /hubs/:id
+server.delete('/hubs/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then(deletedHub => {
+      deletedHub
+        ? res.json(deletedHub)
+        : res.status(404).json({ message: 'invalid hub id' });
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to delete a hub'
+      });
+    });
+});
+
+server.put('/hubs/:id', (req, res) => {
+  const { id } = req.params;
+  const { changes } = req.body;
+
+  db.update(id, changes)
+    .then(updated => {
+      updated
+        ? res.json(updated)
+        : res.status(404).json({ message: 'invalid hub id' });
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to update hub'
+      });
+    });
+});
+
+server.get('hubs/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(hub => {
+      hub
+        ? res.json(hub)
+        : res.status(418).json({ message: 'invalid as heck' });
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to get id'
+      });
     });
 });
 
